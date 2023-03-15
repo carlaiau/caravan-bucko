07cfbe3e4f17024bbd450cda128f037f6fc0d761
Buck Perley <buck@unchained-capital.com>
2023-03-14 20:02:27 -0500

    Updates

---
+++ precache-manifest.51be40b0c340d383f10ad3d5e927a307.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff88fc0d2b03a39f749914ba2c066eb",
+    "url": "/caravan/index.html"
+  },
+  {
+    "revision": "d05f01c8cad35fd48861",
+    "url": "/caravan/static/css/2.d5bec64a.chunk.css"
+  },
+  {
+    "revision": "102f96fd721c1761c909",
+    "url": "/caravan/static/css/main.0527a086.chunk.css"
+  },
+  {
+    "revision": "d05f01c8cad35fd48861",
+    "url": "/caravan/static/js/2.d0239cab.chunk.js"
+  },
+  {
+    "revision": "edb619b315d8bd8d6bf8a17aa804e935",
+    "url": "/caravan/static/js/2.d0239cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "102f96fd721c1761c909",
+    "url": "/caravan/static/js/main.10ecdd4b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/caravan/static/js/main.10ecdd4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "463b8ec66a3715f618ea",
+    "url": "/caravan/static/js/runtime-main.7ddb6876.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/caravan/precache-manifest.5100c0cc63e2948b5656d1025732e4e3.js"
+  "/caravan/precache-manifest.51be40b0c340d383f10ad3d5e927a307.js"
 );
 
 self.addEventListener('message', (event) => {
